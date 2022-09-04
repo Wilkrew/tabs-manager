678a73754d83328614daeed8eddcdf87f4ec2203
Filip Heinel <77172584+Wilkrew@users.noreply.github.com>
2022-09-04 15:35:11 +0200

    MVP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import Moon from "./components/SVG/moon";
+import Sun from "./components/SVG/sun";
 import Button from "react-bootstrap/esm/Button";
 import Card from "react-bootstrap/esm/Card";
 import FormControl from "react-bootstrap/esm/FormControl";
@@ -7,53 +9,92 @@ import InputGroup from "react-bootstrap/esm/InputGroup";
 import TabGroupList from "./components/TabGroupList";
 
 function App() {
-	const [newTabGroupName, setNewTabGroupName] = useState("");
-	const [tabGroupNames, setTabGroupNames] = useState([]);
+  const [newTabGroupName, setNewTabGroupName] = useState("");
+  const [tabGroupNames, setTabGroupNames] = useState([]);
+  const [darkMode, setDarkMode] = useState(true);
 
-	useEffect(() => {
-		chrome.storage.sync
-			.get("tabGroupNames")
-			.then(({ tabGroupNames }) => {
-				setTabGroupNames(tabGroupNames);
-			})
-			.catch(() => {
-				chrome.storage.sync.set({ tabGroupNames: [] });
-			});
-	}, []);
+  const darkModeButtonSize = "24";
 
-	async function getCurrentTabs() {
-		let queryOptions = { windowId: 1 };
-		let tabs = await chrome.tabs.query(queryOptions);
-		return tabs;
-	}
+  //FIXME: dark-mode theme works when adding list items beyond initalt height
+  //FIXME: Wonky Moon svg size
+  //FIXME: Dark mode button positioning
+  //TODO: Save latest used dark/light mode and try to retireve on startup
 
-	function setTabList() {
-		getCurrentTabs().then((response) => {
-			chrome.storage.sync.set({ [newTabGroupName]: response });
-			const newTabGroupNames = [...tabGroupNames, newTabGroupName];
-			setTabGroupNames(newTabGroupNames);
-			chrome.storage.sync.set({ tabGroupNames: newTabGroupNames });
-		});
-	}
+  useEffect(() => {
+    chrome.storage.sync
+      .get("tabGroupNames")
+      .then(({ tabGroupNames }) => {
+        setTabGroupNames(tabGroupNames);
+      })
+      .catch(() => {
+        chrome.storage.sync.set({ tabGroupNames: [] });
+      });
+  }, []);
 
-	return (
-		<Card className="App">
-			<Card.Header>Tabs Manager</Card.Header>
-			<InputGroup className="mb-3">
-				<FormControl
-					name="tabGroupName"
-					onChange={(e) => setNewTabGroupName(e.target.value)}
-					value={newTabGroupName}
-					aria-describedby="basic-addon2"
-				/>
-				<Button onClick={setTabList} variant="outline-secondary" id="button-addon2">
-					New tab group
-				</Button>
-			</InputGroup>
+  async function getCurrentTabs() {
+    let queryOptions = { currentWindow: true };
 
-			<TabGroupList tabGroupNames={tabGroupNames} setTabGroupNames={setTabGroupNames} />
-		</Card>
-	);
+    let tabs = await chrome.tabs.query(queryOptions);
+
+    return tabs;
+  }
+
+  function setTabList() {
+    getCurrentTabs().then((response) => {
+      chrome.storage.sync.set({ [newTabGroupName]: response });
+
+      const newTabGroupNames = [...tabGroupNames, newTabGroupName];
+
+      setTabGroupNames(newTabGroupNames);
+
+      chrome.storage.sync.set({ tabGroupNames: newTabGroupNames });
+    });
+  }
+
+  return (
+    <Card className={`App ${darkMode ? "dark-mode" : "light-mode"}`}>
+      <Card.Header>
+        Tabs Manager{" "}
+        {darkMode ? (
+          <Sun
+            role='button'
+            onClick={() => setDarkMode(!darkMode)}
+            height={darkModeButtonSize}
+            width={darkModeButtonSize}
+            fill='#ffffff'
+          />
+        ) : (
+          <Moon
+            role='button'
+            onClick={() => setDarkMode(!darkMode)}
+            height={darkModeButtonSize}
+            width={darkModeButtonSize}
+          />
+        )}
+      </Card.Header>
+
+      <InputGroup className={`mb-3 ${darkMode ? "dark-mode" : "light-mode"}`}>
+        <FormControl
+          name='tabGroupName'
+          onChange={(e) => setNewTabGroupName(e.target.value)}
+          value={newTabGroupName}
+          aria-describedby='basic-addon2'
+        />
+        <Button
+          onClick={setTabList}
+          variant='outline-secondary'
+          id='button-addon2'
+        >
+          New tab group
+        </Button>
+      </InputGroup>
+
+      <TabGroupList
+        tabGroupNames={tabGroupNames}
+        setTabGroupNames={setTabGroupNames}
+      />
+    </Card>
+  );
 }
 
 export default App;
--- src/components/TabGroupList/TabGroupListItem.js
+++ src/components/TabGroupList/TabGroupListItem.js
@@ -3,25 +3,33 @@ import InputGroup from "react-bootstrap/InputGroup";
 import TrashCanSVG from "./TrashCanSVG";
 
 function TabGroupListItem({ removeTabGroup, tabGroupItem: tabGroupName }) {
-	function openTabs() {
-		chrome.storage.sync.get(tabGroupName).then((response) => {
-			response[tabGroupName].forEach((tab) => {
-				console.log(tab);
-				chrome.tabs.create({ url: tab.url });
-			});
-		});
-	}
+  function openTabs() {
+    chrome.storage.sync.get(tabGroupName).then((response) => {
+      response[tabGroupName].forEach((tab) => {
+        console.log(tab);
+        chrome.tabs.create({ url: tab.url });
+      });
+    });
+  }
 
-	return (
-		<InputGroup className="mb-3 Flex-grow">
-			<Button className="Flex-grow" variant="outline-secondary" onClick={openTabs}>
-				{tabGroupName}
-			</Button>
-			<Button variant="outline-danger" onClick={removeTabGroup}>
-				<TrashCanSVG />
-			</Button>
-		</InputGroup>
-	);
+  return (
+    <InputGroup className='mb-3 Flex-grow'>
+      <Button
+        className='Flex-grow item-height'
+        variant='outline-secondary'
+        onClick={openTabs}
+      >
+        {tabGroupName}
+      </Button>
+      <Button
+        className='item-height'
+        variant='outline-danger'
+        onClick={removeTabGroup}
+      >
+        <TrashCanSVG />
+      </Button>
+    </InputGroup>
+  );
 }
 
 export default TabGroupListItem;
